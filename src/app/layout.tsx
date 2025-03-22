24223f327f85d88cda17baa1c0de5fa489635ceb
jeamxn <admin@chicken-moo.com>
2025-03-22 21:58:29 +0900

    fix: ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className="w-full h-full">
+      <head>
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body className="w-full h-full">
         <div className="w-full h-full flex flex-col items-center justify-center">
           {children}
